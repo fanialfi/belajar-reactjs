419cffda1e9429a8a5c977186df4f581fc0d22c4
fanialfi <fanialfirdaus@gmail.com>
2024-02-27 10:07:27 +0700

    conditional rendering

--- src/app.jsx
+++ src/app.jsx
@@ -1,42 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import Gretting from "./components/grettings";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Gretting isLoggedIn={false}/>)
 
-function Tick(){
-  // statement di bawah disebut state hooks
-  const [clock, setClock] = React.useState(new Date()); // argument yang dimasukkan kedalam function useState disebut inisialisasi nilai awal untuk variable clock
-  
-  try{
-    return (<>
-    <h1>Hello World</h1>
-    <h3>this is at {clock?.toLocaleTimeString()}</h3>
-  </>)
-  }finally{
-    setInterval(() => {
-      setClock(new Date());
-    }, 1000);
-  }
-}
-
-function changeBackground(){
-  const max = 255;
-  let r = Math.floor(Math.random() * max);
-  let g = Math.floor(Math.random() * max);
-  let b = Math.floor(Math.random() * max);
-  document.body.style.backgroundColor = `rgb(${r},${g},${b})`
-}
-
-function Button(){
-  return (
-    <button type="button" onClick={changeBackground}>click</button>
-  )
-}
-
-setInterval(()=>{
-  root.render(<>
-  <Tick date={new Date()}/>
-  <Button />
-  </>)
-},  1000)
+export { root };
---
+++ src/lib/rgb.js
@@ -0,0 +1,7 @@
+export default function randomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+}
