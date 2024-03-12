c9f57f774c552e674aca017e433ba96805e2a2c0
fanialfi <fanialfirdaus@gmail.com>
2024-03-12 12:50:43 +0700

    components dan props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import React from "react";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-4">
+        <ButtonCls />
+        <ButtonFc message={"Hello World"} bgCollor={"bg-red-500"}/>
+      </div>
+    </div>
   )
+}
+
+function ButtonFc({message, bgCollor}){
+  return <button type="submit" className={`h-10 px-6 font-semibold rounded-md ${bgCollor} text-white`}>{message}</button>
+}
+
+class ButtonCls extends React.Component {
+  render(){
+    return <button type="submit" className="h-10 px-6 font-semibold rounded-md bg-slate-100 text-black">Buy Now</button>
+  }
 }
