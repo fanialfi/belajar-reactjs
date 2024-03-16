b8d0c37066c8e4a509533b0eac9e481034e98128
fanialfi <fanialfirdaus@gmail.com>
2024-03-16 14:13:24 +0700

    menampilkan data list

--- src/pages/productsPage.jsx
+++ src/pages/productsPage.jsx
@@ -1,15 +1,33 @@
 import CardProduct from "../components/fragments/cardProduct";
 
+const products = [
+  {
+    id:0,
+    title:"sepatu baru gan",
+    image:"https://source.unsplash.com/300x300?sepatu",
+    description:"lorem ipsum dolor sit amet",
+    price :"Rp. 1.000.000"
+  },{
+    id:1,
+    title:"ini adalah tas sekolah",
+    image:"https://source.unsplash.com/300x300?bag",
+    description:"kamu bisa sekolah dengan menggunakan tas yang kece abis ini",
+    price:"Rp. 200.000"
+  }
+]
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/assets/images/sepatu.jpeg"/>
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore numquam labore enim dolores maxime voluptatibus, voluptatum dicta quasi quod totam eum ipsam ratione velit magnam cumque necessitatibus explicabo repellendus consectetur?
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+        <CardProduct.Header image={product.image}/>
+        <CardProduct.Body title={product.title}>
+          {product.description}
         </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 1.000.000"/>
+        <CardProduct.Footer price={product.price}/>
       </CardProduct>
+      ))}
     </div>
   )
 }
