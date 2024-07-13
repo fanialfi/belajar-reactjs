94ccaa49fc7d6ddc2cc48157697d1cc4370702c5
fanialfi <fanialfirdaus@gmail.com>
2024-07-13 18:19:54 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex items-center min-h-screen justify-center">
+      <div className="p-4 shadow-lg rounded-md ">
+        <h1 className="text-lg font-extrabold tracking-widest drop-shadow-md bg-gradient-to-r from-sky-500 via-indigo-500 to-teal-500 bg-clip-text text-transparent">
+          Hello World
+        </h1>
+        <p className="text-base font-serif tracking-wide text-slate-500">mari belajar reactjs bersama fani</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
